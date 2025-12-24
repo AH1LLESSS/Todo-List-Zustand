2f2885200141438ce1bb950cb56c902a421f6e84
AH1LLESSS <Ah1llesss_game@mail.ru>
2025-12-24 23:14:59 +0300

    no

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+import type { UserConfig } from 'vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Важная настройка:
+  build: {
+    rollupOptions: {
+      input: {
+        // Укажите ТОЧНЫЙ путь к вашему HTML-файлу
+        // Обычно это './index.html'
+        main: './index.html'
+      }
+    }
+  }
 })
